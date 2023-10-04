4937a0dffe5847692bce0238f6efd2454032c46b
Madhur Dhondu Patil <madhurpatilofficial@gmail.com>
2023-10-04 16:14:36 +0530

    git file added

---
+++ Java_with_Github/src/com/training/org/Calculate.java
@@ -0,0 +1,9 @@
+package com.training.org;
+
+public class Calculate {
+	public static void main(String[] args) {
+		System.out.println(3+7);
+	}
+	
+
+}
---
+++ Java_with_Github/src/com/training/org/Welcome.java
@@ -0,0 +1,8 @@
+package com.training.org;
+
+public class Welcome {
+	public static void main(String[] args) {
+		System.out.print("This is just a welcome message");
+	}
+
+}
