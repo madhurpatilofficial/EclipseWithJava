98ec5e44013b02dd7d21fe242ab04b83557378ac
Madhur Patil <madhurpatilofficial@gmail.com>
2023-10-08 17:15:52 +0530

    changes made

--- MyApplication/src/myapp/stringbuild.java
+++
@@ -1,9 +0,0 @@
-package myapp;
-
-public class stringbuild {
-	public static void main(String[] args) {
-		StringBuilder sb = new StringBuilder("Hello World Madhur");
-		System.out.println(sb);
-	}
-
-}
